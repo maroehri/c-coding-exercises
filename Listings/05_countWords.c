03be5b54867113bc2da46180d2fb1e13b9c5ac1f
Martin Roehricht <martin.roehricht@hs-esslingen.de>
2024-04-29 10:53:40 +0200

    Made some minor modifications on the wordCount() function for clarity

--- Listings/05_countWords.c
+++ Listings/05_countWords.c
@@ -5,24 +5,24 @@
 
 int countWords(char string[], char word[]) {
     int lenWord = (int)strlen(word);
-    int count = 0;
-    int i = 0;
+    int wordCount = 0;
+    int stringPos = 0;
 
-    while (string[i + 1] != '\0') {
-        bool validWord = true;
+    while (string[stringPos + 1] != '\0') {
+        bool wordFound = true;
 
         // Stelle sicher, dass das aktuelle Zeichen im String
         // ein alphabetisches Zeichen ist
         // Gehe andernfalls zum nächsten Zeichen im String
-        if (!isalpha(string[i])) {
-            i++;
+        if (!isalpha((unsigned char)string[stringPos])) {
+            stringPos++;
             continue;
         }
 
         // Für ein einzelnes Wort erwarten wir ein Leerzeichen vor dem Wort
         // Ausnahme ist nur das allererste Zeichen des Strings
-        if (i != 0 && string[i - 1] != ' ') {
-            i++;
+        if (stringPos != 0 && string[stringPos - 1] != ' ') {
+            stringPos++;
             continue;
         }
 
@@ -30,30 +30,30 @@ int countWords(char string[], char word[]) {
         // Index im String mit dem jeweiligen Zeichen im Wort übereinstimmt
         // Falls dies an einer Stelle nicht der Fall sein sollte, beenden wir
         // die (innere) Schleife
-        for (int j = 0; j < lenWord; j++) {
-            if (tolower(string[i]) != tolower(word[j])) {
-                validWord = false;
+        for (int wordPos = 0; wordPos < lenWord; wordPos++) {
+            if (tolower(string[stringPos]) != tolower(word[wordPos])) {
+                wordFound = false;
                 break;
             }
             // Die Zeichen waren identisch: gehe einen Schritt weiter
-            i++;
+            stringPos++;
         }
 
         // Nach einem Wort erwarten wir entweder ein Leerraumzeichen oder ein
         // Punktuationszeichen. Wir prüfen also, dass das nächste Zeichen
         // kein alphabetisches Zeichen ist
-        if (isalpha(string[i])) {
-            i++;
+        if (isalpha((unsigned char)string[stringPos])) {
+            stringPos++;
             continue;
         }
 
-        if (validWord)
-            count++;
+        if (wordFound)
+            wordCount++;
 
-        i++;
+        stringPos++;
     }
 
-    return count;
+    return wordCount;
 }
 
 int main(void) {
